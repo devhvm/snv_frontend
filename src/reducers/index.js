35fb938f827c36fb89318a0c5153e9a9de31ea43
NguyenDangKhuong <nguyendangkhuong96@gmail.com>
2019-05-28 22:18:10 +0700

    add rest

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import login from './login'
 import mauphathanh from './mauphathanh'
+import quanLiDonVi from './quanLiDonVi'
 
 export default history =>
   combineReducers({
     router: connectRouter(history),
     login,
-    mauphathanh
+    mauphathanh,
+    quanLiDonVi
   })
---
+++ src/reducers/quanLiDonVi.js
@@ -0,0 +1,35 @@
+import { handleActions, createAction } from 'redux-actions'
+import { rest } from '../utils/rest'
+
+// Action
+export const CO_QUAN_HANH_CHINH = 'CO_QUAN_HANH_CHINH'
+
+// Action Creator
+export const getCoQuanHanhChinh = () => dispatch => {
+  rest
+    .get('common/api/co-quan-chu-tris')
+    .then(res => {
+      dispatch(getCoQuanHanhChinhRequest(res.data))
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+const getCoQuanHanhChinhRequest = createAction(CO_QUAN_HANH_CHINH)
+
+// Initial State
+const initialState = {
+  coQuanHanhChinh: []
+}
+
+// reducer
+export default handleActions(
+  {
+    [CO_QUAN_HANH_CHINH]: (state, { payload }) => ({
+      ...state,
+      coQuanChuTriList: payload
+    })
+  },
+  initialState
+)
