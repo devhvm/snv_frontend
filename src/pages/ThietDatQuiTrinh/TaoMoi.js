116d2b20fe1237d46947e3c74c71b7260a3ed1d1
nguyendangkhuong96 <nguyendang.khuong@asoview.co.jp>
2019-06-18 15:26:29 +0700

    remove collapse

--- src/pages/ThietDatQuiTrinh/TaoMoi.js
+++ src/pages/ThietDatQuiTrinh/TaoMoi.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Col, Form, Row, Modal, Table, Button, Collapse } from 'antd'
+import { Col, Form, Row, Modal, Table, Button } from 'antd'
 import InputItem from '../../components/InputItem'
 
-const Panel = Collapse.Panel
 function TaoMoiThietDatQuiTrinh ({ form, addVisible, setAddVisible }) {
   const columns = [
     {
@@ -90,84 +89,78 @@ function TaoMoiThietDatQuiTrinh ({ form, addVisible, setAddVisible }) {
               />
             </Col>
           </Row>
-          <Collapse>
-            <Panel header='Thêm tiến trình xử lí' key='2'>
-              <Row>
-                <Col span={11}>
-                  <InputItem
-                    form={form}
-                    label='Tiến trình code'
-                    field='tienTrinhCode'
-                    rules={[
-                      {
-                        required: true,
-                        message: 'Vui lòng không để trống thẻ này'
-                      }
-                    ]}
-                  />
-                </Col>
-                <Col span={11} offset={1}>
-                  <InputItem
-                    form={form}
-                    label='Tiến trình code'
-                    field='tienTrinhCode'
-                    rules={[
-                      {
-                        required: true,
-                        message: 'Vui lòng không để trống thẻ này'
-                      }
-                    ]}
-                  />
-                </Col>
-              </Row>
-            </Panel>
-          </Collapse>
-          <Collapse>
-            <Panel header='Thêm tiến trình' key='1'>
-              <Row>
-                <Col span={11}>
-                  <InputItem
-                    form={form}
-                    label='Tiến trình bắt đầu'
-                    field='coQuanHanhChinhCode'
-                    rules={[
-                      {
-                        required: true,
-                        message: 'Vui lòng không để trống thẻ này'
-                      }
-                    ]}
-                  />
-                  <InputItem
-                    form={form}
-                    label='Trạng Thái'
-                    field='coQuanHanhChinhCode'
-                    rules={[
-                      {
-                        required: true,
-                        message: 'Vui lòng không để trống thẻ này'
-                      }
-                    ]}
-                  />
-                </Col>
-                <Col span={11} offset={1}>
-                  <InputItem
-                    form={form}
-                    label='Tiến trình kết thúc'
-                    field='coQuanHanhChinhCode'
-                    rules={[
-                      {
-                        required: true,
-                        message: 'Vui lòng không để trống thẻ này'
-                      }
-                    ]}
-                  />
-                  <Form.Item>
-                    <Button>Xác nhận</Button>
-                  </Form.Item>
-                </Col>
-              </Row>
-            </Panel>
-          </Collapse>
+          <h3>Thêm tiến trình</h3>
+          <Row>
+            <Col span={11}>
+              <InputItem
+                form={form}
+                label='Tiến trình code'
+                field='tienTrinhCode'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Vui lòng không để trống thẻ này'
+                  }
+                ]}
+              />
+            </Col>
+            <Col span={11} offset={1}>
+              <InputItem
+                form={form}
+                label='Tiến trình code'
+                field='tienTrinhCode'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Vui lòng không để trống thẻ này'
+                  }
+                ]}
+              />
+            </Col>
+          </Row>
+          <h3>Thêm tiến trình xử lý</h3>
+          <Row>
+            <Col span={11}>
+              <InputItem
+                form={form}
+                label='Tiến trình bắt đầu'
+                field='coQuanHanhChinhCode'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Vui lòng không để trống thẻ này'
+                  }
+                ]}
+              />
+              <InputItem
+                form={form}
+                label='Trạng Thái'
+                field='coQuanHanhChinhCode'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Vui lòng không để trống thẻ này'
+                  }
+                ]}
+              />
+            </Col>
+            <Col span={11} offset={1}>
+              <InputItem
+                form={form}
+                label='Tiến trình kết thúc'
+                field='coQuanHanhChinhCode'
+                rules={[
+                  {
+                    required: true,
+                    message: 'Vui lòng không để trống thẻ này'
+                  }
+                ]}
+              />
+              <Form.Item>
+                <Button>Xác nhận</Button>
+              </Form.Item>
+            </Col>
+          </Row>
           <Row>
             <Table columns={columns} dataSource={data} />
           </Row>
